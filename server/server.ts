0ef8ed910e90fcd214e6a9b8695d91dbe327a410
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2023-03-21 18:48:26 +0700

    Add config for CORS and remove routing

--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,24 @@
 import * as express from 'express';
+import * as cors from 'cors'
 import * as http from 'http';
 import * as WebSocket from 'ws';
 
 const app = express();
+app.use(cors());
 
 const server = http.createServer(app);
 
-app.get(`/:channel`, (req, res) => {
+const wss = new WebSocket.Server({ server });
 
-  /* Upon enter channel, upgrade the connection to ws */
-  const wss = new WebSocket.Server({ server });
-
-  wss.on('connection', (ws: WebSocket) => {
-    ws.on('message', (message: string) => {
-
-      /* Broadcasting to clients */
-      wss.clients.forEach(client => {
-        client.send(message);
-      })
-    });
+wss.on('connection', (ws: WebSocket) => {
+  ws.on('message', (message: string) => {
+    console.log(message);
+    /* Broadcasting to clients */
+    wss.clients.forEach(client => {
+      client.send(message);
+    })
   });
-})
+});
 
 server.listen(3000, () => {
   console.log(`Server started :)`);
