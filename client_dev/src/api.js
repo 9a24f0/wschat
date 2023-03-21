a12ed9c09ecde9f8c42cc40eed652e303d97ee1d
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2023-03-21 09:03:24 +0700

    Remove client_dev

--- client_dev/src/api.js
+++
@@ -1,9 +0,0 @@
-export const socket = new WebSocket('ws://localhost:3000')
-
-socket.onopen = () => {
-  socket.send("hello Server!")
-}
-
-socket.onmessage = (event) => {
-  socket.send(event.data);
-}
--- client_dev/src/components/ChatHeader.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="h-12 px-8 place-items-center bg-slate-900">
-    <h1 class="text-white text-xl">Hash chat goes here</h1>
-  </div>
-</template>
