0ad3173796ad6979dfe9494329ace97768e195a6
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2023-03-21 16:10:38 +0700

    Add Routing and upgrade ws when route

--- server/server.ts
+++ server/server.ts
@@ -4,30 +4,24 @@ import * as WebSocket from 'ws';
 
 const app = express();
 
-interface Message {
-  content: string,
-  sender: string,
-  senderProfileImgUrl: string
-}
-
-//initialize a simple http server
 const server = http.createServer(app);
 
-//initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
+app.get(`/:channel`, (req, res) => {
 
-wss.on('connection', (ws: WebSocket) => {
-  ws.on('message', (message: string) => {
-    wss.clients.forEach(client => {
-      client.send(message);
-    })
+  /* Upon enter channel, upgrade the connection to ws */
+  const wss = new WebSocket.Server({ server });
+
+  wss.on('connection', (ws: WebSocket) => {
+    ws.on('message', (message: string) => {
+
+      /* Broadcasting to clients */
+      wss.clients.forEach(client => {
+        client.send(message);
+      })
+    });
   });
+})
 
-  //send immediatly a feedback to the incoming connection    
-  ws.send('Hi there, I am a WebSocket server');
-});
-
-//start our server
 server.listen(3000, () => {
   console.log(`Server started :)`);
 });
